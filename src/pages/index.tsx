1151ca0a0ad99a292282d1b84f06e58b23806136
HOSENUR <nur08439@gmail.com>
2023-03-14 02:10:20 +0530

    Created header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
+import Header from "@/components/Header";
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
 
 export default function index() {
   return (
-    <div>
-      <Link href="/blog">blog</Link>
-    </div>
-  )
+    <>
+      <Head>
+        <title>nur&apos;s blog</title>
+      </Head>
+      <Header />
+      <hr />
+      <div className="max-w-5xl mx-auto p-5">
+        <Link href="/blog">blog</Link>
+      </div>
+    </>
+  );
 }
