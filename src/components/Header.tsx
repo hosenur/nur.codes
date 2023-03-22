7c0252ac1df2d774b6edb1607b35617e45d39cfe
HOSENUR <nur08439@gmail.com>
2023-03-22 21:52:47 +0530

    Glassmorphed Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 export default function Header() {
   return (
-    <div className="max-w-5xl p-5 mx-auto flex items-center justify-between font-medium text-xl">
-      <Link href="/">nur.codes</Link>
+    <div className=" fixed w-full z-50 glass  font-medium text-xl">
+      <div className="max-w-5xl mx-auto p-5 flex justify-between items-center">
+        <Link href="/">nur.codes</Link>
+      </div>
     </div>
   );
 }
