9bf6425abee0359ee8dec80df18cbb267fa67559
HOSENUR <nur08439@gmail.com>
2023-03-22 22:26:13 +0530

    Style Fixes

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -53,10 +53,11 @@ export default function Blog({ data }: { data: any }) {
   return (
     <>
       <Head>
-        <title>Blog</title>
+        <title>{data.title}</title>
         <meta name="description" content={data.description} />
         <meta property="og:title" content={data.title} />
         <meta property="og:description" content={data.description} />
+        <meta property="og:image" content={data.cover} />
       </Head>
       <Header />
       <div className="max-w-5xl pt-24 mx-auto p-5 flex flex-col space-y-5">
